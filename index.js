252502ff378caa44e6cd2d02cdfe0dfdb47376b8
Kagami <124534196+HyperQR@users.noreply.github.com>
2025-09-10 14:50:45 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// Wed, 10 Sept 2025
+
+var Age = prompt("Enter your age: ");
+console.log(`Your are ${Age} years old`);
+
+// parseInt
+console.log("Year after");
+var Age = parseInt(Age) + 1;
+console.log(`Your are ${Age} years old`);
