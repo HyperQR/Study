56d783e86635a051ea68cc80622d2a65b9b36917
Kagami <124534196+HyperQR@users.noreply.github.com>
2025-09-15 13:51:39 +0100

    Update listing.js

--- listing.js
+++ listing.js
@@ -9,3 +9,14 @@ console.log(`There are ${amount} Students`)
 
 // loging last students
 console.log(`Last students is ${Students[amount-1]}`)
+
+// Adding item to list
+const Cars = [];
+console.log(`The amount of items: ${Cars.length}`)
+Cars.push("BMW")
+console.log(`The amount of items: ${Cars.length}`)
+console.log(Cars)
+
+let newCars = prompt("Add a car: ")
+Cars.push(newCars)
+console.log(Cars)
