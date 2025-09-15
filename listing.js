acd9aa3679679126a648664c0d3b4627a61feed4
Kagami <124534196+HyperQR@users.noreply.github.com>
2025-09-15 11:55:40 +0100

    Create listing.js

---
+++ listing.js
@@ -0,0 +1,11 @@
+//listing
+const Students = ["Darpan", "Khushal", "Dev"];
+console.log(Students);
+console.log(`first students is: ${Students[1]}`);
+
+// List amount
+let amount = Students.length;
+console.log(`There are ${amount} Students`)
+
+// loging last students
+console.log(`Last students is ${Students[amount-1]}`)
