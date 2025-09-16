2e792f850b537f0d204ada187d24cb5f11b958b8
Kagami <124534196+HyperQR@users.noreply.github.com>
2025-09-16 13:28:26 +0100

    Create function.js

---
+++ function.js
@@ -0,0 +1,15 @@
+function Greet(name){
+    console.log(`Hello ${name}`)
+}
+
+function Productinfo(){
+    let name = "shirt";
+    let price = 50.99
+    console.log(`- ${name} - ${price}`)
+}
+
+
+Greet("bob");
+Greet("mark")
+Productinfo()
+    
