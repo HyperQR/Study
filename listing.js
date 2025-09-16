64c876450008767c3516ccb9bd0a9f0db40291a3
Kagami <124534196+HyperQR@users.noreply.github.com>
2025-09-16 09:48:57 +0100

    Update listing.js

--- listing.js
+++ listing.js
@@ -20,3 +20,19 @@ console.log(Cars)
 let newCars = prompt("Add a car: ")
 Cars.push(newCars)
 console.log(Cars)
+
+let testList = [ "item1", "item2", "item3", "item4", "item5",]
+
+let index = testList.indexOf("new item")
+console.log(`item 4 is at index ${index}`)
+
+testList.splice(2,2,"new item")
+console.log(testList)
+
+
+// All the list function
+listname.push() // Add to end of list 
+listname.pop() // remove from the end list 
+listname.unshift() //add to front of list 
+listname.shift() //remove from the front of list 
+Listname.splice() remove a item from the list  
