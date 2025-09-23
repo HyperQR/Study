4264e4dbcddb72e1684e4dc1a8b1b5b4ae1902a7
Kagami <124534196+HyperQR@users.noreply.github.com>
2025-09-23 09:48:21 +0100

    Update if.js

--- if.js
+++ if.js
@@ -10,3 +10,13 @@ if(name != "bob"){
 console.log("Program Ended")
 
 
+let a = 34;
+
+if(a < 20){
+    console.log("Less than 20")
+}else if(a < 50){
+     console.log("Less than 50")
+}else if(a < 100){
+     console.log("Less than 100")
+}
+
