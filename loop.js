196dd7c23ad381a4b700aac0bc20b9d9f4d2f3cb
Kagami <124534196+HyperQR@users.noreply.github.com>
2025-09-24 14:33:23 +0100

    Update loop.js

--- loop.js
+++ loop.js
@@ -21,10 +21,22 @@ console.log("program end")
 
 let count = 0;
 let num = Number(prompt("enter the number: "))
-
 while( count < num){
     console.log("Hi")
     count = count + 1;
 }
 
+// sir example 
+let amountOfLoops = Number(prompt("Enter amount of loops:"));
 
+let doneSoFar = 0;
+while(doneSoFar < amountOfLoops){
+console.log("Hi");
+
+// doneSoFar = doneSoFar + 1
+
+// doneSoFar += 1; up
+
+doneSoFar++;
+
+}
