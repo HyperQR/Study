697f8ea14794efb814cec412d1ab47ee424f0f61
Kagami <124534196+HyperQR@users.noreply.github.com>
2025-09-24 09:54:13 +0100

    Update loop.js

--- loop.js
+++ loop.js
@@ -8,20 +8,13 @@ while( count < 10){
 
 console.log("Program end")
 
- let people []:
-
+ 
+let people = [];
 console.log(people);
-
-lvt choice prompt("Add a person? y/n: ");
-
-- while(choice "y"){ //loop starts, essentially an if statement but rapoate
-
-people.push(prompt("Enter new person name: "))
-
-choice prompt("Add a person? y/n 3
-
-21//block ends, code will go hack to Line s
-
-9 console.log(people);
-
-10 console.log("program end")
+let choice = prompt("Add a person? y/n: ");
+while(choice == "y"){ //loop starts essentiall an if statement but rapoate
+    people.push(prompt("Enter new person name:"));
+    choice = prompt("Add a person? y/n");
+} //block ends, code will go hack to Line s
+console.log(people);
+console.log("program end")
