cd1df1da00907bda31cffa88f73a741ac2d0ea40
Kagami <124534196+HyperQR@users.noreply.github.com>
2025-09-29 15:36:09 +0100

    Create homework.js

---
+++ homework.js
@@ -0,0 +1,11 @@
+// get the difference between number a and b and add that to the second number for return
+
+// if number a is bigger than number b use subtraction instead
+
+function bob(num1, num2){
+    if(){
+        
+    }
+}
+
+bob();
