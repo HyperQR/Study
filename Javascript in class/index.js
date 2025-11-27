da0d6f8e7fa414e004171fbb8c35bb7695207c1f
Darpan Soma <Darpansoma@gmail.com>
2025-11-27 14:32:23 +0000

    local storage

---
+++ Javascript in class/index.js
@@ -0,0 +1,15 @@
+// Alert the websit
+// alert("Hello World")
+
+
+// Local storage
+
+
+let username = localStorage.getItem("username")
+
+alert(username)
+
+username = prompt("Enter Name: ")
+localStorage.setItem("username", username)
+
+
